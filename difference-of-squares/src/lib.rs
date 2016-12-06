30059d2bfdd9ddcb209633ff722e0726d3205d4e
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2016-12-06 18:31:31 +0100

    some .map() magic, and suddenly it works! (Type hints rock!)

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,9 +1,12 @@
 pub fn square_of_sum(num: usize) -> usize {
-  225
+  // type-hints for the win!
+  //   "::<usize>" tells the compiler what comes out of our range.
+  //   since the range can be converted to basically any numeric type, we need to help
+  (1..num+1).sum::<usize>().pow(2)
 }
 
 pub fn sum_of_squares(num: usize) -> usize {
-  55
+  (1..num+1).map(|x| x.pow(2)).sum()
 }
 
 pub fn difference(num: usize) -> usize {
--- difference-of-squares/tests/difference-of-square.rs
+++ difference-of-squares/tests/difference-of-square.rs
@@ -16,19 +16,16 @@ fn test_difference_5() {
 }
 
 #[test]
-#[ignore]
 fn test_square_of_sum_100() {
     assert_eq!(25502500, squares::square_of_sum(100));
 }
 
 #[test]
-#[ignore]
 fn test_sum_of_squares_100() {
     assert_eq!(338350, squares::sum_of_squares(100));
 }
 
 #[test]
-#[ignore]
 fn test_difference_100() {
     assert_eq!(25164150, squares::difference(100));
 }
