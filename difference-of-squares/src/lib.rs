9b84d907809506dcd9b46592b3fb1f733d28bf9f
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2016-12-06 17:34:27 +0100

    7) diff-of-squares: first thing that compiles, but unimplemented!() everywhere

---
+++ difference-of-squares/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn square_of_sum(num: usize) -> usize {
+  unimplemented!();
+}
+
+pub fn sum_of_squares(num: usize) -> usize {
+  unimplemented!();
+}
+
+pub fn difference(num: usize) -> usize {
+  unimplemented!();
+}
