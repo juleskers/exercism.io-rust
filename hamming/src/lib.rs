8f7747ba78f80369f657fb539215dd66a7f32a83
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2016-12-10 21:30:20 +0100

    hamming: first thing that gives a correct answer

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn hamming_distance(l: &str, r: &str) -> Result<u16, ()> {
-  return Ok(0);
+  let mut distance = 0;
+  for (el, er) in l.chars().zip(r.chars()) {
+    if el != er { distance += 1; }
+  }
+  
+  return Ok(distance);
 }
--- hamming/tests/hamming.rs
+++ hamming/tests/hamming.rs
@@ -6,13 +6,11 @@ fn test_no_difference_between_empty_strands() {
 }
 
 #[test]
-#[ignore]
 fn test_no_difference_between_identical_strands() {
     assert_eq!(hamming::hamming_distance("GGACTGA", "GGACTGA").unwrap(), 0);
 }
 
 #[test]
-#[ignore]
 fn test_complete_hamming_distance_in_small_strand() {
     assert_eq!(hamming::hamming_distance("ACT", "GGA").unwrap(), 3);
 }
