23efebbad88eff559ff14ff0783aa474d4a6c161
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2016-12-13 08:03:48 +0100

    laying groundwork for more rows: interesting compiler error: variable  from pattern #2 is not bound in pattern #1, Huh?

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -3,14 +3,20 @@ pub struct PascalsTriangle {
 }
 
 impl PascalsTriangle {
-    pub fn new(row_count: u32) -> Self {
+    pub fn new(row_count: usize) -> Self {
         // prepare the Pascal triangle
         // We know how many rows we'll have, save some Vec re-allocations with that knowledge
-        let mut rb = Vec::with_capacity(row_count as usize);
+        let mut rb = Vec::with_capacity(row_count);
 
         // Fill the rows, one by one
         for i in 1..row_count+1 {
-            rb.push(vec![1])
+            // prepare the current row
+            let new_row = Vec::with_capacity(i); // It's a triangle: each row is as long as it is down
+            match i {
+              1 | row_count => new_row.push(1), // first and last item of a row are always '1'
+            }
+            
+            rb.push(new_row)
         }
 
         // wrap our new triangle in a PT struct for returning
