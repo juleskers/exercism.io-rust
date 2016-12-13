ce6e5510be811d57858cd77f2818816686a5c98c
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2016-12-13 08:16:40 +0100

    wonderful compiler error: '_' is an unreachable pattern. Very helpful though: explains that my 'match i { row_count => ..' doesn't do what I expect, instead binds all values

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -11,9 +11,19 @@ impl PascalsTriangle {
         // Fill the rows, one by one
         for i in 1..row_count+1 {
             // prepare the current row
-            let new_row = Vec::with_capacity(i); // It's a triangle: each row is as long as it is down
+            let mut new_row = Vec::with_capacity(i); // It's a triangle: each row is as long as it is down
+            
+            // fill the current row
             match i {
-              1 | row_count => new_row.push(1), // first and last item of a row are always '1'
+               // first and last item of a row are always '1', because they're on the edge, with an "n/a" and a "1" above them
+               1         => new_row.push(1),
+               // DWIM error: doesn't Do What I Mean: doesn't compare against parameter row_count,
+               // instead matches any value, and binds that to a new "row_count", shadowing the parameter
+               row_count => new_row.push(1),
+               // all other rows are sum of previous elements above them
+               // COMPILER ERROR: this is unreachable, because everything else is already captured into the row_count branch
+               // Hats off to rustc exhaustiveness-checking!
+               _ => unimplemented!(),
             }
             
             rb.push(new_row)
