b8c87ea2917dae5e63e8fd0f0a63706e3e4e21bc
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2016-12-13 07:59:13 +0100

    Ok, now we can do 0 and 1 rows; still no logic for more, yet

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -4,12 +4,16 @@ pub struct PascalsTriangle {
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
+        // prepare the Pascal triangle
+        // We know how many rows we'll have, save some Vec re-allocations with that knowledge
         let mut rb = Vec::with_capacity(row_count as usize);
-        for i in 1..row_count {
-          rb.push(vec![1])
+
+        // Fill the rows, one by one
+        for i in 1..row_count+1 {
+            rb.push(vec![1])
         }
 
-
+        // wrap our new triangle in a PT struct for returning
         PascalsTriangle {
             row_buffer: rb
         }
