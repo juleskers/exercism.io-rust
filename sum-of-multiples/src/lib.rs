3801f31c9b6a961d9c8684a97b6d16799037cdea
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-12 22:13:25 +0100

    fix the second type error: be lazy and just specify we turn a ref to a number; convoluted, but not wrong?

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,3 @@
-pub fn sum_of_multiples(limit: u32, to_multiply: &[u32]) -> u32 {
+pub fn sum_of_multiples(limit: u32, to_multiply: &[u32]) -> &u32 {
   to_multiply.iter().filter(|n| n < limit ).next().unwrap()
 }
