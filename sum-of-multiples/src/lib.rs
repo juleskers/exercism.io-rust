bbbcc0f33364fcb77f618a370bad813f774848aa
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-12 22:43:06 +0100

    introduce a match to make both tests pass (as long as we don't have more than one number)

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn sum_of_multiples(limit: u32, to_multiply: &[u32]) -> u32 {
-  *(to_multiply.iter().filter(|n| n < &&limit ).next().unwrap())
+  match to_multiply.iter().filter(|n| n < &&limit ).next() {
+    Some(v) => *v,
+    None    =>  0,
+  }
 }
