cb13b61085cc1622dfadba0f64d29885ad9216d2
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-13 09:05:52 +0100

    Nice compiler error (great note!): my functional-fu is not yet strong enough. Time to fiddle with conversions :-)
    Build failed, waiting for other jobs to finish...
    error[E0277]: the trait bound `std::vec::Vec<u32>: std::iter::FromIterator<std::iter::Map<std::slice::Iter<'_, u32>, [closure@src/lib.rs:11:28: 11:35 r:_]>>` is not satisfied
      --> src/lib.rs:12:6
       |
    12 |   }).collect();
       |      ^^^^^^^ trait `std::vec::Vec<u32>: std::iter::FromIterator<std::iter::Map<std::slice::Iter<'_, u32>, [closure@src/lib.rs:11:28: 11:35 r:_]>>` not satisfied
       |
       = note: a collection of type `std::vec::Vec<u32>` cannot be built from an iterator over elements of type `std::iter::Map<std::slice::Iter<'_, u32>, [closure@src/lib.rs:11:28: 11:35 r:_]>`

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,4 +1,15 @@
 pub fn sum_of_multiples(limit: u32, to_multiply: &[u32]) -> u32 {
+  let to_multiply_max = to_multiply.iter().max().unwrap();
+
+  // as u32 +1: we round up, because a to_multiply lower-than-max
+  //  may stay below the limit while the max goes over the limit.
+  //  to compensate, we filter in a later stage
+  let repeat_limit = (limit/to_multiply_max) as u32 + 1; 
+
+  let all_multiples: Vec<u32> = (1..repeat_limit+1).map(|r| {
+    to_multiply.iter().map(|m| m*r)
+  }).collect();
+
   match to_multiply.iter().filter(|n| n < &&limit ).next() {
     Some(v) => *v,
     None    =>  0,
