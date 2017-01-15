26463eca07a04ca2e79ab8a58aacfbf146a1b742
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-15 13:18:58 +0100

    Use the std, Luke! Instead of a Vec that we would need to de-duplicate, use a Set, which already deduplicates on insertion: The power of the std-lib :-)

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, to_multiply: &[u32]) -> u32 {
   let to_multiply_min = to_multiply.iter().min().unwrap();
 
@@ -6,14 +8,14 @@ pub fn sum_of_multiples(limit: u32, to_multiply: &[u32]) -> u32 {
   //  to compensate, we filter in a later stage
   let repeat_limit = (limit/to_multiply_min) as u32 + 1;
 
-  let mut all_multiples: Vec<u32> = Vec::new();
+  let mut all_multiples: HashSet<u32> = HashSet::new();
   for r in (1..repeat_limit+1) {
     println!("repeat {}", r);
     for m in to_multiply {
       let candidate = r*m;
       println!("  candidate: {}", candidate);
       if candidate < limit {
-        all_multiples.push(candidate);
+        all_multiples.insert(candidate);
         println!("    added!");
       }
     }
