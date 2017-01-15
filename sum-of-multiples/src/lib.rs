8d91ecda8554fba96c8bd1d8f566240957650f82
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-15 13:08:08 +0100

    aha, our numbers are too low! We are skipping the higher multiples of the lower numbers! increase our repeat_limit to compensate. This still fails the test though, why?

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,10 +1,10 @@
 pub fn sum_of_multiples(limit: u32, to_multiply: &[u32]) -> u32 {
-  let to_multiply_max = to_multiply.iter().max().unwrap();
+  let to_multiply_min = to_multiply.iter().min().unwrap();
 
   // as u32 +1: we round up, because a to_multiply lower-than-max
   //  may stay below the limit while the max goes over the limit.
   //  to compensate, we filter in a later stage
-  let repeat_limit = (limit/to_multiply_max) as u32 + 1; 
+  let repeat_limit = (limit/to_multiply_min) as u32 + 1;
 
   let mut sum: u32 = 0;
   for r in (1..repeat_limit+1) {
