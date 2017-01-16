56797df1af4d95bfb1c7df2a4c46cb3eb5ed234c
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-16 09:25:59 +0100

    Switch to lowercase, and all the dominoes that entailed:
    - to_lowercase() turns our &str into a fully-fledged String..
    - ..meaning we can't compare to "a" anymore (which is a 'static str)
    - Since we have to switch to single characters at some point, throw in a ".chars()"..
    - ..That makes our comparison easier: compare to 'a' instead of "a"
    - since chars() returns a sequence, take the first element (using a dirty-dirty .next().unwrap() )

--- scrabble-score/src/lib.rs
+++ scrabble-score/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn score(word: &str) -> u16 {
-  match word {
-    "a" => 1,
-    "f" => 4,
+  match word.to_lowercase().chars().next().unwrap() {
+    'a' => 1,
+    'f' => 4,
     _   => unimplemented!(),
   }
 }
