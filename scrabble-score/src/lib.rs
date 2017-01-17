e669cf7d4b3e987195c94c1348e5166e63014230
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-17 09:33:08 +0100

    Oh yeah! just remembered Rust is an 'expression language', so match can return a value! Stop repeating 'score +=' all the time, DRY! :-D

--- scrabble-score/src/lib.rs
+++ scrabble-score/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn score(word: &str) -> u16 {
   let mut score = 0;
   for the_char in word.to_lowercase().chars() {
-    match the_char {
-      'a'|'e'|'i'|'o'|'u'|'n'|'r'|'s'|'t' => score += 1,
-      'b'|'p' => score += 3,
-      'f'|'h'|'y' => score += 4,
-      'k' => score += 5,
-      'x' => score += 8,
-      'q'|'z' => score += 10,
+    score += match the_char {
+      'a'|'e'|'i'|'o'|'u'|'n'|'r'|'s'|'t' => 1,
+      'b'|'p' => 3,
+      'f'|'h'|'y' => 4,
+      'k' => 5,
+      'x' => 8,
+      'q'|'z' => 10,
       _   => unimplemented!(),
     }
   }
