a5ff1939f7b11b532e1b47b12402efcfb700fdce
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-27 21:02:32 +0100

    more refactoring, with my first external crate usage\!
    We use Boolinator to get the same combinator awesomeness for the sequence check.
    Because my brain kept confusing itself with the boolean logic of any(valid....is_err), reword some things to be clearer

--- nucleotide-count/src/lib.rs
+++ nucleotide-count/src/lib.rs
@@ -2,22 +2,25 @@
 // experiences and conversations with the compiler at my github diary "Learning Rust":
 // https://github.com/juleskers/exercism.io-rust/tree/master/nucleotide-count/src/lib.rs
 
+extern crate boolinator;
+
 use std::collections::HashMap;
+use boolinator::Boolinator;
 
 pub fn count(nucleotide: char, sequence: &str)-> Result<usize, String> {
 
-    valid_nucleotide(nucleotide).or (
+    validate(nucleotide).or (
         Err(format!("Invalid nucleotide '{}', expected one of A, T, G or C", nucleotide))
     )?;
 
-    if sequence.chars().any(|s| valid_nucleotide(s).is_err()) {
-        return Err(format!("Invalid nucleotide in sequence: '{}', expected one of A, T, G or C", nucleotide));
-    }
+    sequence.chars().all(|s| validate(s).is_ok()).ok_or(
+        format!("Invalid nucleotide in sequence: '{}', expected one of A, T, G or C", nucleotide)
+    )?;
 
     Ok(0)
 }
 
-fn valid_nucleotide(nuc: char) -> Result<(), ()> {
+fn validate(nuc: char) -> Result<(), ()> {
     if "ATGC".contains(nuc) {
         Ok(())
     } else {
