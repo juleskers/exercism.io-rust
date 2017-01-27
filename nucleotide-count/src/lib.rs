32466b60209ceadd7e96f2581ea4a7f1b9f85bce
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-27 19:48:07 +0100

    It compiles! finally! (needed to use/include HashMap

--- nucleotide-count/src/lib.rs
+++ nucleotide-count/src/lib.rs
@@ -2,6 +2,8 @@
 // experiences and conversations with the compiler at my github diary "Learning Rust":
 // https://github.com/juleskers/exercism.io-rust/tree/master/nucleotide-count/src/lib.rs
 
+use std::collections::HashMap;
+
 pub fn count(nucleotide: char, sequence: &str)-> Result<usize, String> {
     unimplemented!();
 }
