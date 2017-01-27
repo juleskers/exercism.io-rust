6bc386c97078406c000cac3a86f11ae3b6ad837d
Jules Kerssemakers <jules.kerssemakers@gmail.com>
2017-01-27 17:51:19 +0100

    add a template for new exercises

---
+++ template.rs
@@ -0,0 +1,7 @@
+// Follow the complete history of this file, including all my painful learning
+// experiences and conversations with the compiler at my github diary "Learning Rust":
+// https://github.com/juleskers/exercism.io-rust
+
+fn foo() {
+
+}
