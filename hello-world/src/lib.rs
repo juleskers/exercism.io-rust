15e5af20227cc12edb215b4cb08e7b8664e43738
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-10 18:59:59 +0800

    Delete hello-world directory

--- hello-world/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub fn hello(input: Option<&str>) -> String {
-  match input {
-    Some(name) => return format!("Hello, {}!", name).to_string(),
-    None       => return "Hello, World!".to_string(),
-  }
-}
-
