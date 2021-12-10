e4c1cf4d867e262db36b21be90c52e3abf893611
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-10 18:58:17 +0800

    Delete gigasecond directory

--- gigasecond/src/lib.rs
+++
@@ -1,8 +0,0 @@
-extern crate chrono;
-use chrono::*;
-
-pub fn after(birthday: chrono::DateTime<UTC>) -> chrono::DateTime<UTC> {
-  let gigasecond = Duration::seconds(1_000_000_000);
-  
-  birthday.checked_add(gigasecond).unwrap()
-}
