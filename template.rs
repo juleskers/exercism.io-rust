fee6ff8229658c54197d0b8811d2e19d47080887
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-10 19:36:23 +0800

    Delete template.rs

--- template.rs
+++
@@ -1,7 +0,0 @@
-// Follow the complete history of this file, including all my painful learning
-// experiences and conversations with the compiler at my github diary "Learning Rust":
-// https://github.com/juleskers/exercism.io-rust
-
-fn foo() {
-
-}
